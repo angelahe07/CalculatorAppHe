5b6909ffd69926a4379e05730d512bf45ce4dedc
angelahe07 <90780383+angelahe07@users.noreply.github.com>
2023-09-10 18:10:37 -0500

    finished functionality of all buttons except parentheses
    currently, input only limited to 2 numbers

--- app/src/main/java/com/example/calculatorapphe/MainActivity.java
+++ app/src/main/java/com/example/calculatorapphe/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.calculatorapphe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,57 +17,108 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // numImput gathers the data on which number button is clicked
-    int numInput;
-    // operation gathers the data on which operation is being done
-    String operation;
-    // symbol gathers the data on which symbol (parentheses, decimal) is being used,
-    String symbol;
-    String numberInputFull1;
-    String numberInputFull2;
+    // numInput1 is the first number in the expression
+    double numInput1;
+    // numInput2 is the second number in the expression
+    double numInput2;
+    String numberInputFull = "";
 
+    double result;
     public void buttonSelected(View v){
+        TextView input1TV = findViewById(R.id.textViewInputExpression);
         if(v.getId() == R.id.button0){
-            numInput = 0;
-            if(numberInputFull1.isEmpty() || numberInputFull2.isEmpty()){
-                numberInputFull1 += "";
+            if(numberInputFull.isEmpty()){
+                numberInputFull += "";
+            }else{
+                numberInputFull += 0;
             }
         }else if(v.getId() == R.id.button1){
-            numInput = 1;
+            numberInputFull += 1;
         }else if(v.getId() == R.id.button2){
-            numInput = 2;
+            numberInputFull += 2;
         }else if(v.getId() == R.id.button3){
-            numInput = 3;
+            numberInputFull += 3;
         }else if(v.getId() == R.id.button4){
-            numInput = 4;
+            numberInputFull += 4;
         }else if(v.getId() == R.id.button5){
-            numInput = 5;
+            numberInputFull += 5;
         }else if(v.getId() == R.id.button6){
-            numInput = 6;
+            numberInputFull += 6;
         }else if(v.getId() == R.id.button7){
-            numInput = 7;
+            numberInputFull += 7;
         }else if(v.getId() == R.id.button8){
-            numInput = 8;
+            numberInputFull += 8;
         }else if(v.getId() == R.id.button9){
-            numInput = 9;
-        }else if(v.getId() == R.id.buttonBack){
-            numInput = 0;
+            numberInputFull += 9;
+        }else if(v.getId() == R.id.buttonClear){
+            numberInputFull = "";
         }else if(v.getId() == R.id.buttonDecimal){
-            symbol = ".";
-        }else if(v.getId() == R.id.buttonDivide){
-            operation = "÷";
+            numberInputFull += ".";
         }else if(v.getId() == R.id.buttonLeftParentheses){
-            symbol = "(";
+            numberInputFull += "(";
         }else if(v.getId() == R.id.buttonRightParentheses){
-            symbol = ")";
+            numberInputFull += ")";
+        }else if(v.getId() == R.id.buttonDivide){
+            if(!isOperationDuplicate()){
+                numberInputFull += " ÷ ";
+                numInput1 = Double.valueOf(numberInputFull.substring(0, numberInputFull.length() - 2));
+            }else{
+                numberInputFull += "";
+            }
         }else if(v.getId() == R.id.buttonMultiply){
-            operation = "×";
+            if(!isOperationDuplicate()){
+                numberInputFull += " × ";
+                numInput1 = Double.valueOf(numberInputFull.substring(0, numberInputFull.length() - 2));
+            }else{
+                numberInputFull += "";
+            }
         }else if(v.getId() == R.id.buttonNegative){
-            numInput *= -1;
+            numberInputFull += -1;
         }else if(v.getId() == R.id.buttonPlus){
-            operation = "+";
+            if(!isOperationDuplicate()){
+                numberInputFull += " + ";
+                numInput1 = Double.valueOf(numberInputFull.substring(0, numberInputFull.length() - 2));
+            }else{
+                numberInputFull += "";
+            }
         }else if(v.getId() == R.id.buttonSubtract){
-            operation = "-";
+            if(!isOperationDuplicate()){
+                numberInputFull += " - ";
+                numInput1 = Double.valueOf(numberInputFull.substring(0, numberInputFull.length() - 2));
+            }else{
+                numberInputFull += "";
+            }
+        }else if(v.getId() == R.id.buttonEnter){
+            if(numberInputFull.indexOf("÷") != -1){
+                numInput2 = Double.valueOf(numberInputFull.substring(numberInputFull.indexOf("÷") + 2));
+                result = numInput1/numInput2;
+                numberInputFull += " = " + result;
+                input1TV.setText(numberInputFull);
+            }else if(numberInputFull.indexOf("×") != -1) {
+                numInput2 = Double.valueOf(numberInputFull.substring(numberInputFull.indexOf("×") + 2));
+                result = numInput1 * numInput2;
+                numberInputFull += " = " + result;
+                input1TV.setText(numberInputFull);
+            }else if(numberInputFull.indexOf("+") != -1){
+                numInput2 = Double.valueOf(numberInputFull.substring(numberInputFull.indexOf("+") + 2));
+                result = numInput1 + numInput2;
+                numberInputFull += " = " + result;
+                input1TV.setText(numberInputFull);
+            }else if(numberInputFull.indexOf("-") != -1){
+                numInput2 = Double.valueOf(numberInputFull.substring(numberInputFull.indexOf("-") + 2));
+                result = numInput1 - numInput2;
+                numberInputFull += " = " + result;
+                input1TV.setText(numberInputFull);
+            }
+        }
+        input1TV.setText(numberInputFull);
+    }
+    public boolean isOperationDuplicate(){
+        if(numberInputFull.indexOf("+") != -1 || numberInputFull.indexOf("-") != -1
+                || numberInputFull.indexOf("×") != -1 || numberInputFull.indexOf("÷") != -1){
+            return true;
+        }else{
+            return false;
         }
     }
 }
