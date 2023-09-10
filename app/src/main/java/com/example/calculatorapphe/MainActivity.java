6f42b970c83145df1ea2e6445258aeb2f7f8bcc2
angelahe07 <90780383+angelahe07@users.noreply.github.com>
2023-09-10 15:44:18 -0500

    working on functionality of buttons

--- app/src/main/java/com/example/calculatorapphe/MainActivity.java
+++ app/src/main/java/com/example/calculatorapphe/MainActivity.java
@@ -16,14 +16,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // numImput gathers the data on which number button is clicked
-    double numInput;
+    int numInput;
     // operation gathers the data on which operation is being done
     String operation;
-    // symbol gathers the data on which symbol (parentheses) is being used,
+    // symbol gathers the data on which symbol (parentheses, decimal) is being used,
     String symbol;
+    String numberInputFull1;
+    String numberInputFull2;
+
     public void buttonSelected(View v){
         if(v.getId() == R.id.button0){
             numInput = 0;
+            if(numberInputFull1.isEmpty() || numberInputFull2.isEmpty()){
+                numberInputFull1 += "";
+            }
         }else if(v.getId() == R.id.button1){
             numInput = 1;
         }else if(v.getId() == R.id.button2){
@@ -36,6 +42,30 @@ public class MainActivity extends AppCompatActivity {
             numInput = 5;
         }else if(v.getId() == R.id.button6){
             numInput = 6;
+        }else if(v.getId() == R.id.button7){
+            numInput = 7;
+        }else if(v.getId() == R.id.button8){
+            numInput = 8;
+        }else if(v.getId() == R.id.button9){
+            numInput = 9;
+        }else if(v.getId() == R.id.buttonBack){
+            numInput = 0;
+        }else if(v.getId() == R.id.buttonDecimal){
+            symbol = ".";
+        }else if(v.getId() == R.id.buttonDivide){
+            operation = "÷";
+        }else if(v.getId() == R.id.buttonLeftParentheses){
+            symbol = "(";
+        }else if(v.getId() == R.id.buttonRightParentheses){
+            symbol = ")";
+        }else if(v.getId() == R.id.buttonMultiply){
+            operation = "×";
+        }else if(v.getId() == R.id.buttonNegative){
+            numInput *= -1;
+        }else if(v.getId() == R.id.buttonPlus){
+            operation = "+";
+        }else if(v.getId() == R.id.buttonSubtract){
+            operation = "-";
         }
     }
 }
